a760b50baa7f6d27dc4749f6749db357e65eaa91
JGr1ff1n <JGr1ff1n@users.noreply.github.com>
2020-10-15 15:28:42 -0500

    Current working DeviceInit

--- DeviceInitialization.py
+++ DeviceInitialization.py
@@ -15,27 +15,27 @@ import cv2
 import os
 
 #path for USB drive
-usbPath = "~/media/VIDEOS"
+usbPath = "/media/pi/VIDEOS"
 #path for local storage
-localPath = "~/Documents/localVids"
+localPath = "/home/pi/Documents/localVids"
 
 #A1.2 - detects if the face camera (usb Camera) is accessible for recording
 def detectFaceCam():
-options_webcam = {"iso": 100, "exposure_compensation": 0, "awb_mode": "sun", "sensor_mode": 0, "CAP_PROP_FRAME_WIDTH ":1920, "CAP_PROP_FRAME_HEIGHT":1080, "CAP_PROP_AUTOFOCUS": 'True'} # define tweak parameters
+    options_webcam = {"exposure_compensation": 0, "awb_mode": "sun", "sensor_mode": 0, "CAP_PROP_FRAME_WIDTH ":1920, "CAP_PROP_FRAME_HEIGHT":1080, "CAP_PROP_AUTOFOCUS": 'True'} # define tweak parameters
 
-try :
-    video_streams = VideoGear(source=0, resolution=(1920,1080), **options_webcam).start()
-    print("FaceCam Detected")
-    video_stream.stop()
-except:
-    ErrorHandling.errorFaceCam()
+    try:
+        video_stream = VideoGear(source=0, resolution=(1920,1080), **options_webcam).start()
+        print("FaceCam Detected")
+        video_stream.stop()
+    except:
+        ErrorHandling.errorFaceCam()
 
 #A1.3 - detects if the tablet camera (pi Camera) is accessible for recording
 def detectTabletCam():
 
-    options_picam = {"iso": 1800, "exposure_compensation": 15, "awb_mode": "horizon", "sensor_mode": 0, "CAP_PROP_FRAME_WIDTH ":1920, "CAP_PROP_FRAME_HEIGHT":1080}
+    options_picam = {"exposure_compensation": 15, "awb_mode": "horizon", "sensor_mode": 0, "CAP_PROP_FRAME_WIDTH ":1920, "CAP_PROP_FRAME_HEIGHT":1080}
 
-    try :
+    try:
         video_stream = VideoGear(source=2, resolution=(1920,1080), **options_picam).start()
         print("TabletCam Detected")
         video_stream.stop()
@@ -55,27 +55,28 @@ def detectExternalUSB():
 #A1.5 - detects if the external storage device has enough available video storage
 def verifyStorageCapacity():
 
-    try :
+    try:
         disk = os.statvfs(usbPath)
         availableSpaceMB = (disk.f_bfree * disk.f_bsize /1024/ 1024)
         print("Storage Available: %.3f MB" % (availableSpaceMB))
-        if(availableSpaceMB > 200)
-            print(External Storage Space Adequate)
-        else
+        if(availableSpaceMB > 30000):
+            print("External Storage Space Adequate")
+        else:
             ErrorHandling.errorUSBStorage()
     except:
         ErrorHandling.errorUSBStorage()
 
 #A1.6 - prepares device storage for recording - deletes previous recordings and prepares folder
 def prepLocalStorage():
-    if(os.path.exists(localPath))
+    if(os.path.exists(localPath)):
         for file in os.listdir(localPath):
-            #os.remove(file)
-            #maybe have to add localPath to file disk_name
             print(file)
-    else
-        os.mkdir(path)
-    print("Local Storage " + localPath + "prepared")
+            if(file.endswith(".txt")):
+                print("removed")
+                os.system("sudo rm "+localPath+"/"+file)
+    else:
+        os.system("sudo mkdir " + localPath)
+    print("Local Storage " + localPath + " prepared")
 
 #A1.7 - changes LED to green to indicate device is ready to record.
 def finishInitialization():
@@ -84,9 +85,10 @@ def finishInitialization():
     print("Initialization Complete")
 
 #Calls initialiation functions in order
-detectFaceCam()
-detectTabletCam()
-detectExternalUSB()
-verifyStorageCapacity()
-prepLocalStorage()
+
+#detectFaceCam()
+#detectTabletCam()
+#detectExternalUSB()
+#verifyStorageCapacity()
+#prepLocalStorage()
 finishInitialization()
